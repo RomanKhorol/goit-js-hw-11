017fe0cf3b5f6b8e58aaeb3a2c58511b5d9ccbd2
RomanKhorol <Rom4uk691@gmail.com>
2023-01-21 19:50:05 +0200

    fixedAllTaskWithoutInfiniteScroll

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 const axios = require('axios').default;
+
 const refs = {
   form: document.querySelector('.search-form'),
   input: document.querySelector('.search-form input'),
@@ -12,59 +13,68 @@ const refs = {
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.btnLoadMore.addEventListener('click', onBtnKoadMoreClick);
+refs.input.addEventListener('input', onInputChange);
+refs.btnLoadMore.style.display = 'none';
 let numberPage = 1;
 
 function onFormSubmit(evt) {
   evt.preventDefault();
   refs.gallery.innerHTML = '';
-  numberPage = 0;
-  const pictureName = refs.input.value;
+
+  const pictureName = refs.input.value.trim();
   if (pictureName === '') {
     refs.btnLoadMore.style.display = 'none';
     return;
   }
-  getPictureArray(pictureName);
+  getPictureArray(pictureName).then(imageProcessing);
 }
-async function getPictureArray(name) {
-  numberPage += 1;
 
+async function getPictureArray(name) {
   try {
     const response = await axios.get(
       `https://pixabay.com/api/?key=32890609-fac98cfe2238792085833a9d0&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${numberPage}`
     );
 
-    if (response.data.hits.length === 0) {
-      Notiflix.Notify.warning(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-      return;
-    }
-    refs.btnLoadMore.style.display = 'flex';
-    let numberOfHits = response.data.totalHits;
-    if (numberPage > 1) {
-      Notiflix.Notify.success(`Hooray! We found ${numberOfHits} images.`);
-    }
-    const pictures = response.data.hits;
-    const renPict = renderPictures(pictures);
-    refs.gallery.insertAdjacentHTML('beforeend', renPict);
+    return response;
+  } catch (error) {
+    refs.btnLoadMore.style.display = 'none';
+    Notiflix.Notify.warning(
+      'Were sorry, but youve reached the end of search results.'
+    );
+  }
+}
+
+function imageProcessing(response) {
+  if (response.data.hits.length === 0) {
+    Notiflix.Notify.warning(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+
+  let numberOfHits = response.data.totalHits;
+  if (numberPage === 1) {
+    Notiflix.Notify.success(`Hooray! We found ${numberOfHits} images.`);
+  }
+  const pictures = response.data.hits;
+  const renPict = renderPictures(pictures);
+  refs.gallery.insertAdjacentHTML('beforeend', renPict);
+  var lightbox = new SimpleLightbox('div.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  lightbox.refresh();
+  refs.btnLoadMore.style.display = 'flex';
+  if (numberPage > 1) {
     const { height } = document
       .querySelector('.gallery')
       .firstElementChild.getBoundingClientRect();
     window.scrollBy({
-      top: height * 2,
+      top: height * 1.5,
       behavior: 'smooth',
     });
-    var lightbox = new SimpleLightbox('div.gallery a', {
-      captionsData: 'alt',
-      captionDelay: 250,
-    });
-    lightbox.refresh();
-  } catch (error) {
-    Notiflix.Notify.warning(
-      'Were sorry, but youve reached the end of search results.'
-    );
-    refs.btnLoadMore.style.display = 'none';
   }
+  numberPage += 1;
 }
 
 function renderPictures(pictures) {
@@ -97,9 +107,18 @@ function renderPictures(pictures) {
 }
 
 function onBtnKoadMoreClick(event) {
-  const pictureName = refs.input.value;
-
   refs.btnLoadMore.style.display = 'none';
-  getPictureArray(pictureName);
+  const pictureName = refs.input.value.trim();
+  getPictureArray(pictureName).then(imageProcessing);
+  const { height } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: height * 2,
+    behavior: 'smooth',
+  });
   refs.btnLoadMore.style.display = 'flex';
+}
+function onInputChange(e) {
+  numberPage = 1;
 }
